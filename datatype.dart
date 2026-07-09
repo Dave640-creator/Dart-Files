299bc0ae178f5950cb364a53cc6a80050e2d13f9
Dave Lausa <davelausa113@gmail.com>
2026-07-09 21:15:48 +0800

    Add files via upload

---
+++ datatype.dart
@@ -0,0 +1,28 @@
+void main() {
+  String name;
+  name = "Dave";
+
+  print(name.length);
+  print(name.toUpperCase());
+  print(name.contains("ave"));
+
+  String age = "22";
+  print(age);
+
+  int ages = int.parse(age);
+
+  print(ages);
+
+  int number = 10;
+
+  String nums = number.toString();
+
+  print(nums);
+
+  double price = 10.09;
+
+  int finalPrice = price.toInt();
+
+  print(finalPrice);
+  print(price);
+}
