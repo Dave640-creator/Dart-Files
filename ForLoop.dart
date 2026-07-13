1cd4044ab5fec3aca02f1b54b63b48223810bce4
Dave Lausa <davelausa113@gmail.com>
2026-07-13 23:15:49 +0800

    Add files via upload

--- ForLoop.dart
+++ ForLoop.dart
@@ -4,26 +4,31 @@ void main() {
   print("Input a Number: ");
   int count = 0;
   int number = int.parse(stdin.readLineSync()!);
-  /*
-  for (int i = 1; i <= number; i++) {
-    if (i % 2 == 0) {
-      print(i);
-      count++;
-    }
-  }
-  count = evenChecker(count, number);
-  print("There is $count even numbers");
-  */
+
   String even = evenChecker(count, number).toString();
   print("There is $even even numbers");
 }
 
 int evenChecker(int count, int number) {
-  for (int i = 1; i <= number; i++) {
+  /* for (int i = 1; i <= number; i++) {
     if (i % 2 == 0) {
       print(i);
       count++;
     }
+  }
+  return count;
+
+
+
+  */
+
+  
+  while (i <= number) {
+    if (i % 2 == 0) {
+      print(i);
+      count++;
+    }
+    i++;
   }
   return count;
 }
