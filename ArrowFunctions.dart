4cdf5949da5a36573bd022a9f30bfe7ef32cf7e1
Dave Lausa <davelausa113@gmail.com>
2026-07-15 20:29:23 +0800

    Add files via upload

---
+++ ArrowFunctions.dart
@@ -0,0 +1,14 @@
+//import "dart:io";
+
+//  dart ArrowFunctions.dart
+
+void main() {
+  print(arrowAdd(5, 10));
+}
+
+int add(int a, int b) {
+  return a + b;
+}
+
+int arrowAdd(int a, int b) => a + b;
+int arrowMinus(int a, int b) => a - b;
